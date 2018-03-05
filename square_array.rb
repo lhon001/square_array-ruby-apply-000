a4c710050391002d6223c131d8544272a3f16b4c
Lawrence Hon <lhon001@gmail.com>
2018-03-05 23:16:28 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,3 @@
 def square_array(array)
-  squared_array = []
-  array.each do |number|
-    squared_array << number ** 2 
-  end
-  squared_array
+  return array.collect {|number| number * number}
 end
