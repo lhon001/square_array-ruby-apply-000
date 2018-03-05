56a23d95109a1a57e58557ae43b8d0d137075354
Lawrence Hon <lhon001@gmail.com>
2018-03-05 23:24:17 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
-  squared_array = []
-  array.each do |number| 
-    squared_array << (number * number)
+  #squared_array = []
+  array.each {|number| squared_array << (number * number)}
   end
   squared_array
 end
